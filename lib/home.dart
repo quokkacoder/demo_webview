ac4ebd93fe9bb7e86db7c7663884f2b7578220a1
JSilver <hoangvt979@gmail.com>
2020-04-15 14:12:03 +0700

    init project

--- lib/home.dart
+++ lib/home.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-import 'loading_widget.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomePage extends StatelessWidget {
   final _url = 'http://hn.bep365.vn/login.php';
+  WebViewController _webViewController;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: WebviewScaffold(
-          url: _url,
-          withZoom: true,
-          withLocalStorage: true,
-          hidden: true,
-          initialChild: Container(
-            color: Colors.white,
-            child: LoadingWidget(),
-          )),
+    return Scaffold(
+      body: WebView(
+        onWebViewCreated: (controller) {
+          _webViewController = controller;
+        },
+        initialUrl: _url,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: ()=>_webViewController?.reload()
+      ),
     );
   }
 }
